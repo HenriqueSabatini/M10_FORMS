99a922e2ad263a5279ea45ffa73d636d50af3e02
Henrique Sabatini <134343903+HenriqueSabatini@users.noreply.github.com>
2024-01-08 22:35:52 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$('#telefone').mask('(00) 00000-0000',{ 
+    placeholder: '(__) _____-____'
+})
+
+$('#CEP').mask('00000-000',{ 
+    placeholder: '_____-___'
+})
+
+$('form').validate({ 
+    rules: {
+        nome: {
+            required: true
+        },
+        email: {
+            required: true,
+            email: true
+        },
+        telefone: {
+            required: true
+        },
+        endereco: {
+            required: true
+        },
+        CEP: {
+            required: true
+        }                    
+    },
+    messages:{ //aqui definimos as mensagens obrigatorias em cada campo
+        nome: 'Por favor, insira seu nome'
+    }, 
+    //abaixo vamos adicionar um pop-up para o usuario mostrando quantos campos estao incorretos no formulario
+    submitHandler: function(form) {
+        console.log(form)
+    },
+    invalidHandler: function(evento, validador) {
+        let camposIncorretos = validador.numberOfInvalids();
+        if (camposIncorretos) {
+            alert(`Existem ${camposIncorretos} campos incorretos`)
+        }
+    }
+})
